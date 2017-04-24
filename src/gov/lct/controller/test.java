934069a819705e31be1e9d42d591d1593134da0f
cm8295 <cm8295@163.com>
2017-04-24 21:52:56 +0800

    df

--- src/gov/lct/controller/test.java
+++ src/gov/lct/controller/test.java
@@ -11,7 +11,7 @@ public class test {
 	
 	@RequestMapping("/test1")
 	public String test1(HttpServletRequest request) throws Exception {
-		System.err.println("nihao--testing-branch");
+		System.err.println("nihao--testing-bra");
 		return "/index";
 	}
 	
